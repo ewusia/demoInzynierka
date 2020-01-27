01553dd5c72c27482d78c1aa993ae7f97b820ef9
ewusia <Staszica4m61>
2020-01-27 22:30:18 +0100

    refactor testów

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -7,9 +7,6 @@ import com.ea.inzynierka.exception.CategoryNotFound;
 import com.ea.inzynierka.model.Author;
 import com.ea.inzynierka.model.Book;
 import com.ea.inzynierka.model.Category;
-import com.ea.inzynierka.repo.AuthorRepository;
-import com.ea.inzynierka.repo.BookRepository;
-import com.ea.inzynierka.repo.CategoryRepository;
 import com.ea.inzynierka.service.*;
 import org.junit.Assert;
 import org.junit.Ignore;
@@ -28,187 +25,183 @@ import java.util.Set;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 @Transactional
-public class DemoApplicationTests {
+public class DemoApplicationTests extends AbstractDemoApplicationTests {
+
+	private final static String IT = "IT";
+	private final static String HISTORY = "History";
+	private final static String CRIMINAL = "Criminal";
+	private final static String CAY_S_HORSTMANN = "Cay S. Horstmann";
+	private final static String NORMAN_DAVIES = "Norman Davies";
+	private static final String AGATHA_CHRISTIE = "Agatha Christie";
+	private static final String JAVA_PODSTAWY = "Java. Podstawy";
+	private static final String HISTORIA_SWIATA = "Historia Świata";
+	private static final String HARD_COVER = "hard-cover";
+	private static final String YEAR = "2019";
 
 	@Autowired
 	private CategoryService categoryService;
 	@Autowired
 	private AuthorService authorService;
 	@Autowired
-	private BookRepository bookRepository;
-	@Autowired
 	private BookService bookService;
 
+	@Override
+	protected Long getId(Category category) {
+		Long id = category.getId();
+		Assert.assertNotNull(id);
+		return id;
+	}
+	@Override
+	protected Long getId(Author author) {
+		Long id = author.getId();
+		Assert.assertNotNull(id);
+		return id;
+	}
+	@Override
+	protected Long getId(Book book) {
+		Long id = book.getId();
+		Assert.assertNotNull(id);
+		return id;
+	}
+	private void findCategoryById(long id, String categoryName) {
+		Optional<Category> category = categoryService.findById(id);
+		Assert.assertEquals(categoryName, category.get().getName());
+	}
+	private void findAuthorById(long id, String authorName) {
+		Optional<Author> author = authorService.findById(id);
+		Assert.assertEquals(authorName, author.get().getName());
+	}
+	private void findBookById(long id) {
+		Optional<Book> book = Optional.ofNullable(bookService.findById(id));
+		Assert.assertEquals(JAVA_PODSTAWY, book.get().getTitle());
+		Assert.assertEquals(IT, book.get().getCategory().getName());
+		Assert.assertEquals(HARD_COVER, book.get().getCover());
+		Assert.assertEquals(YEAR, book.get().getYear());
+		Assert.assertEquals(CAY_S_HORSTMANN, book.get().getAuthors().stream().findFirst().get().getName());
+	}
+	private Category addCategory(String categoryName) {
+		Category category = new Category();
+		category.setName(categoryName);
+		categoryService.save(category);
+		return category;
+	}
+	private Author addAuthor(String authorName) {
+		Author author = new Author();
+		author.setName(authorName);
+		authorService.save(author);
+		return author;
+	}
+	private Book addBook(Category category, Author author, String title, String year, String cover) {
+		Set authors = new HashSet();
+		authors.add(author);
+		Book book = new Book();
+		book.setTitle(title);
+		book.setYear(year);
+		book.setCover(cover);
+		book.setCategory(category);
+		book.setAuthors(authors);
+		bookService.save(book);
+		return book;
+	}
 	@Test
 	public void contextLoads() {
 	}
 	@Test
-	public void testFindCategoryById()
-	{
-		Optional<Category> category = categoryService.findById(-1);
-		Assert.assertEquals("IT", category.get().getName());
+	public void testFindCategoryById() {
+		findCategoryById(-1, IT);
+		return;
+	}
+	@Test
+	public void testFindAuthorById() {
+		findAuthorById(-1, CAY_S_HORSTMANN);
 		return;
 	}
 	@Test
 	//@Rollback(false)
-	public void testAddCategory()
-	{
-		Category category = new Category();
-		category.setName("History");
-		categoryService.save(category);
-		Long id = category.getId();
-		Assert.assertNotNull(id);
+	public void testAddCategory() {
+		Category category = addCategory(HISTORY);
+		Long id = getId(category);
 		Optional<Category> newCategory = categoryService.findById(id);
-
-		Assert.assertEquals("History", newCategory.get().getName());
+		Assert.assertEquals(HISTORY, newCategory.get().getName());
+		return;
+	}
+	@Test
+	//@Rollback(false)
+	public void testAddAuthor() {
+		Author author = addAuthor(NORMAN_DAVIES);
+		Long id = getId(author);
+		Optional<Author> newAuthor = authorService.findById(id);
+		Assert.assertEquals(NORMAN_DAVIES, newAuthor.get().getName());
 		return;
 	}
 	@Test
 	public void testRemoveCategory() throws CategoryNotFound {
 		long startingNumberOfCategory = categoryService.count();
-		Category category = new Category();
-		category.setName("Criminal");
-		categoryService.save(category);
-		Long id = category.getId();
-		Assert.assertNotNull(id);
+		Category category = addCategory(CRIMINAL);
+		Long id = getId(category);
 		Optional<Category> newCategory = categoryService.findById(id);
-
-		Assert.assertEquals("Criminal", newCategory.get().getName());
+		Assert.assertEquals(CRIMINAL, newCategory.get().getName());
 		Assert.assertEquals(4, startingNumberOfCategory+1);
-
 		categoryService.delete(id);
 		Assert.assertEquals(3, startingNumberOfCategory);
 	}
 	@Test
-	public void testFindAuthorById()
-	{
-		Optional<Author> author = authorService.findById(-1);
-		Assert.assertEquals("Cay S. Horstmann", author.get().getName());
-		return;
-	}
-	@Test
-	//@Rollback(false)
-	public void testAddAuthor()
-	{
-		Author author = new Author();
-		author.setName("Norman Davies");
-		authorService.save(author);
-		Long id = author.getId();
-		Assert.assertNotNull(id);
-		Optional<Author> newAuthor = authorService.findById(id);
-
-		Assert.assertEquals("Norman Davies", newAuthor.get().getName());
-		return;
-	}
-	@Test
 	public void testRemoveAuthor() throws AuthorNotFound {
 		long startingNumberOfAuthor = authorService.count();
-		Author author = new Author();
-		author.setName("Agatha Christie");
-		authorService.save(author);
-		Long id = author.getId();
-		Assert.assertNotNull(id);
+		Author author = addAuthor(AGATHA_CHRISTIE);
+		Long id = getId(author);
 		Optional<Author> newAuthor = authorService.findById(id);
-
 		Assert.assertEquals("Agatha Christie", newAuthor.get().getName());
 		Assert.assertEquals(6, startingNumberOfAuthor+1);
-
 		authorService.delete(id);
 		Assert.assertEquals(5, startingNumberOfAuthor);
 	}
-	//@Ignore
 	@Test
-	public void testFindBookById()
-	{
-		Optional<Book> book = Optional.ofNullable(bookService.findById(-1));
-		Assert.assertEquals("Java. Podstawy", book.get().getTitle());
-		Assert.assertEquals("IT", book.get().getCategory().getName());
-		Assert.assertEquals("hard-cover", book.get().getCover());
-		Assert.assertEquals("2019", book.get().getYear());
-		Assert.assertEquals("Cay S. Horstmann", book.get().getAuthors().stream().findFirst().get().getName());
+	public void testFindBookById() {
+		findBookById(-1);
 		return;
 	}
-	//@Ignore
 	@Test
-	//@Rollback(false)
-	public void testAddBook()
-	{
-		Category category = new Category();
-		category.setName("History");
-		categoryService.save(category);
-		Long idc = category.getId();
-		Assert.assertNotNull(idc);
+	public void testAddBook() {
+		Category category = addCategory(HISTORY);
+		Long idc = getId(category);
 		Optional<Category> newCategory = categoryService.findById(idc);
+		Assert.assertEquals(HISTORY, newCategory.get().getName());
 
-		Assert.assertEquals("History", newCategory.get().getName());
-
-		Author author = new Author();
-		author.setName("Norman Davies");
-		authorService.save(author);
-		Long ida = author.getId();
-		Assert.assertNotNull(ida);
+		Author author = addAuthor(NORMAN_DAVIES);
+		Long ida = getId(author);
 		Optional<Author> newAuthor = authorService.findById(ida);
+		Assert.assertEquals(NORMAN_DAVIES, newAuthor.get().getName());
 
-		Assert.assertEquals("Norman Davies", newAuthor.get().getName());
-
-		Set authors = new HashSet();
-		authors.add(author);
-
-		Book book = new Book();
-		book.setTitle("Historia Świata");
-		book.setYear("2008");
-		book.setCover("hard-cover");
-		book.setCategory(category);
-		book.setAuthors(authors);
-		bookService.save(book);
-
-		Long idb = book.getId();
-		Assert.assertNotNull(idb);
+		Book book = addBook(category, author, HISTORIA_SWIATA, YEAR, HARD_COVER);
+		Long idb = getId(book);
 		Optional<Book> newBook = Optional.ofNullable(bookService.findById(idb));
 
-		Assert.assertEquals("Historia Świata", newBook.get().getTitle());
-		Assert.assertEquals("2008", newBook.get().getYear());
-		Assert.assertEquals("hard-cover", newBook.get().getCover());
-		Assert.assertEquals("History", newBook.get().getCategory().getName());
-		Assert.assertEquals("Norman Davies", newBook.get().getAuthors().stream().findFirst().get().getName());
+		Assert.assertEquals(HISTORIA_SWIATA, newBook.get().getTitle());
+		Assert.assertEquals(YEAR, newBook.get().getYear());
+		Assert.assertEquals(HARD_COVER, newBook.get().getCover());
+		Assert.assertEquals(HISTORY, newBook.get().getCategory().getName());
+		Assert.assertEquals(NORMAN_DAVIES, newBook.get().getAuthors().stream().findFirst().get().getName());
 		return;
 	}
+
 	@Test
 	public void testRemoveBook() throws BookNotFound {
 		long startingNumberOfBook = bookService.count();
-		Category category = new Category();
-		category.setName("History");
-		categoryService.save(category);
-		Long idc = category.getId();
-		Assert.assertNotNull(idc);
+
+		Category category = addCategory(HISTORY);
+		Long idc = getId(category);
 		Optional<Category> newCategory = categoryService.findById(idc);
+		Assert.assertEquals(HISTORY, newCategory.get().getName());
 
-		Assert.assertEquals("History", newCategory.get().getName());
-
-		Author author = new Author();
-		author.setName("Norman Davies");
-		authorService.save(author);
-		Long ida = author.getId();
-		Assert.assertNotNull(ida);
+		Author author = addAuthor(NORMAN_DAVIES);
+		Long ida = getId(author);
 		Optional<Author> newAuthor = authorService.findById(ida);
+		Assert.assertEquals(NORMAN_DAVIES, newAuthor.get().getName());
 
-		Assert.assertEquals("Norman Davies", newAuthor.get().getName());
-
-		Set authors = new HashSet();
-		authors.add(author);
-
-		Book book = new Book();
-		book.setTitle("Historia Świata");
-		book.setYear("2008");
-		book.setCover("hard-cover");
-		book.setCategory(category);
-		book.setAuthors(authors);
-		bookService.save(book);
-
-		Long idb = book.getId();
-		Assert.assertNotNull(idb);
+		Book book = addBook(category, author, HISTORIA_SWIATA, YEAR, HARD_COVER);
+		Long idb = getId(book);
 		Assert.assertEquals(6, startingNumberOfBook+1);
-
 		bookService.delete(idb);
 		Assert.assertEquals(5, startingNumberOfBook);
 	}
